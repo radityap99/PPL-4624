97ced4e5fe8cc552b282feb2a0e6104196108f11
Laurentius Krisna Yoga <80138087+krisnayg@users.noreply.github.com>
2021-06-16 14:27:34 +0700

    Add files via upload

--- application/controllers/admin/update_menu.php
+++ application/controllers/admin/update_menu.php
@@ -16,12 +16,16 @@ class update_menu extends CI_Controller{
         $keterangan = $this->input->post('keterangan');
         $harga      = $this->input->post('harga');
         $gambar     = $_FILES['gambar']['name'];
+
         if($gambar = ''){}else{
             $config ['upload_path'] = './uploads';
             $config ['allowed_type'] = 'jpg|jpeg|png|gif';
+            $config ['file_name']   = $this->uploads;
+            $config ['overwrite']   = true;
 
             $this->load->library('upload', $config);
-            if(!$this->upload->do_upload('gambar')){
+
+            if($this->upload->do_upload('gambar')){
                 echo "Gambar gagal diupload";
             }else{
                 $gambar=$this->upload->data('file_name');
@@ -36,6 +40,43 @@ class update_menu extends CI_Controller{
         );
 
         $this->model_menu->tambahmenu($data, 'tb_menu');
+        redirect('admin/update_menu/index');
+
+        return "default.jpg";
+    }
+
+    public function edit($id)
+    {
+        $where = array ('id_menu' =>$id);
+        $data['menu'] = $this->model_menu->edit_menu($where, 'tb_menu')->result();
+
+        $this->load->view('templates_admin/header');
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/edit_menu', $data);
+        $this->load->view('templates_admin/footer');
+    }
+
+    public function update(){
+        $id             =$this->input->post('id_menu');
+        $nama_menu      =$this->input->post('nama_menu');
+        $keterangan     =$this->input->post('keterangan');
+        $harga          =$this->input->post('harga');
+
+        $data = array(
+            'nama_menu'     => $nama_menu,
+            'keterangan'    => $keterangan,
+            'harga'         => $harga,
+        );
+        $where = array(
+            'id_menu'   => $id
+        );
+        $this->model_menu->update_data($where,$data,'tb_menu');
+        redirect('admin/update_menu/index');
+    }
+
+    public function hapus ($id){
+        $where = array('id_menu' => $id);
+        $this->model_menu->hapus_data($where, 'tb_menu');
         redirect('admin/update_menu/index');
     }
 }
