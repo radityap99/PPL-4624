dfd009d5a6daed6ecc0544401c30cfa9528c3f05
Laurentius Krisna Yoga <80138087+krisnayg@users.noreply.github.com>
2021-06-23 16:13:10 +0700

    Add files via upload

--- application/controllers/history.php
+++ application/controllers/history.php
@@ -9,5 +9,15 @@ class History extends CI_Controller{
         $this->load->view('admin/history',$data);
         $this->load->view('templates_admin/footer');
     }
+
+    public function detail($id_history)
+    {
+        $data['history'] = $this->model_history->ambil_id_history($id_history);
+        $data['pesanan'] = $this->model_history->ambil_id_pesanan($id_history);
+        $this->load->view('templates_admin/header');
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/detail_history',$data);
+        $this->load->view('templates_admin/footer');
+    }
 }
 ?>
