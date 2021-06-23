9ea499b66dc3563847e0f72f9792e1e5e557d787
Laurentius Krisna Yoga <80138087+krisnayg@users.noreply.github.com>
2021-06-23 18:55:50 +0700

    Add files via upload

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('model_menu','model_history','model_auth','model_laporan');
+$autoload['model'] = array('model_menu','model_history','model_auth','model_laporan','model_kategori');
