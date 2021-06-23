9124124abe58ed5de4f96c7d2d6cc3c62abf85da
Laurentius Krisna Yoga <80138087+krisnayg@users.noreply.github.com>
2021-06-23 18:59:26 +0700

    Add files via upload

---
+++ application/models/model_kategori.php
@@ -0,0 +1,18 @@
+<?php
+
+class Model_kategori extends CI_Model{
+
+    public function data_makanan(){
+        return $this->db->get_where("tb_menu",array('kategori' => 'makanan'));
+    }
+
+    public function data_minuman(){
+        return $this->db->get_where("tb_menu",array('kategori' => 'minuman'));
+    }
+
+    public function data_snack(){
+        return $this->db->get_where("tb_menu",array('kategori' => 'snack'));
+    }
+}
+
+?>
